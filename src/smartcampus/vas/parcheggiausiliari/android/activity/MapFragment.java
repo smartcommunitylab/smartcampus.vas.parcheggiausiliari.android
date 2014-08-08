e6f49f070ea5922491489abfb74e215b2c6c9990
armaDio <armellinimichele@live.it>
2014-08-08 17:00:11 +0200

    added possibility to check if a clicked point is on the polyline added possibility to decode a google encoded polyline

--- src/smartcampus/vas/parcheggiausiliari/android/activity/MapFragment.java
+++ src/smartcampus/vas/parcheggiausiliari/android/activity/MapFragment.java
@@ -1,9 +1,9 @@
 package smartcampus.vas.parcheggiausiliari.android.activity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.osmdroid.bonuspack.overlays.FolderOverlay;
-import org.osmdroid.bonuspack.overlays.Polygon;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
@@ -22,14 +22,11 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MapFragment extends Fragment {
 
@@ -89,22 +86,6 @@ public class MapFragment extends Fragment {
 		GPSTracker pos = new GPSTracker(getActivity());
 		map.getController().setZoom(18);
 		center(new GeoPoint(pos.getLatitude(), pos.getLongitude()));
-		// map.addMarkerOnLongClick();
-
-		/*
-		 * map.getOverlays().add(new LongPressOverlay(getActivity()) {
-		 * 
-		 * @Override public void onLongPressGesture(MotionEvent event) {
-		 * Log.e("", "ASDF"); IGeoPoint point =
-		 * map.getProjection().fromPixels((int)event.getX(), (int)event.getY());
-		 * Toast.makeText(getActivity(),OSMGeocoding .FromPointToAddress( new
-		 * GeoPoint(point.getLatitudeE6(), point.getLongitudeE6()),
-		 * getActivity()).get(0).getAddressLine(0) , Toast.LENGTH_LONG).show();
-		 * showPopup( map, "VIA", OSMGeocoding .FromPointToAddress( new
-		 * GeoPoint(point.getLatitudeE6(), point.getLongitudeE6()),
-		 * getActivity()).get(0).getAddressLine(0)); } });
-		 */
-
 		ArrayList<ParkingMarker> items = new ArrayList<ParkingMarker>();
 		for (Parking mPark : new AusiliariHelper(getActivity()).getParklist()) {
 			ParkingMarker item = new ParkingMarker(
@@ -133,28 +114,9 @@ public class MapFragment extends Fragment {
 							}
 						}, map.getResourceProxy()));
 		map.getOverlays().add(myLoc);
-		/*
-		 * PathOverlay a = new PathOverlay(Color.RED, getActivity()){
-		 * 
-		 * @Override public boolean onLongPress(MotionEvent e, MapView mapView)
-		 * { // TODO Auto-generated method stub Log.d("DEBUG","Pressed road");
-		 * return super.onLongPress(e, mapView); } }; a.addPoint(new
-		 * GeoPoint(new
-		 * AusiliariHelper(getActivity()).getParklist()[0].getPosition()[0],new
-		 * AusiliariHelper(getActivity()).getParklist()[0].getPosition()[1]));
-		 * a.addPoint(new GeoPoint(46.07,11.15));
-		 */
-		MyPolyline a = new MyPolyline(getActivity()) {
-			@Override
-			public boolean onSingleTapUp(MotionEvent e, MapView mapView) {
-				// TODO Auto-generated method stub
-				if(isOnLine(map.getProjection().fromPixels((int)e.getX(),(int)e.getX())))
-					// Log.d("DEBUG", "Path Pressed");
-					Toast.makeText(getActivity(), "PATH PRESSED", Toast.LENGTH_SHORT).show();
-				return super.onLongPress(e, mapView);
-			}
-		};
-
+		
+		
+		/*MyPolyline a = new MyPolyline(getActivity()) ;
 		ArrayList<GeoPoint> list = new ArrayList<GeoPoint>();
 		list.add(new GeoPoint(
 				new AusiliariHelper(getActivity()).getParklist()[0]
@@ -164,12 +126,9 @@ public class MapFragment extends Fragment {
 		list.add(new GeoPoint(46.078, 11.15));
 		list.add(new GeoPoint(46.07, 11.154));
 		a.setPoints(list);
-		FolderOverlay kmlOverlay = new FolderOverlay(getActivity());
-		kmlOverlay.add(a);
-		map.getOverlays().add(kmlOverlay);
-		map.invalidate();
-		// map.getOverlays().add(a);
-
+		map.getOverlays().add(a);
+		map.invalidate();*/
+		debugLinee(map);
 		return rootView;
 	}
 
@@ -212,4 +171,103 @@ public class MapFragment extends Fragment {
 		}
 	}
 
+	
+	
+	private void debugLinee(MapView mv){
+		ArrayList<GeoPoint> a = new ArrayList<GeoPoint>();
+		ArrayList<GeoPoint> b = new ArrayList<GeoPoint>();
+		ArrayList<GeoPoint> c = new ArrayList<GeoPoint>();
+		ArrayList<GeoPoint> d = new ArrayList<GeoPoint>();
+		ArrayList<GeoPoint> e = new ArrayList<GeoPoint>();
+		ArrayList<GeoPoint> f = new ArrayList<GeoPoint>();
+		ArrayList<GeoPoint> g = new ArrayList<GeoPoint>();
+		ArrayList<GeoPoint> h = new ArrayList<GeoPoint>();
+		ArrayList<GeoPoint> i = new ArrayList<GeoPoint>();
+		ArrayList<GeoPoint> j = new ArrayList<GeoPoint>();
+				
+		
+		a.add(new GeoPoint(46.091802, 11.115442));
+		a.add(new GeoPoint(46.085313, 11.118746));
+		b.add(new GeoPoint(46.083468, 11.122695));
+		b.add(new GeoPoint(46.085194, 11.122780));
+		b.add(new GeoPoint(46.086385, 11.121622));
+		c.add(new GeoPoint(46.086385, 11.121622));
+		c.add(new GeoPoint(46.086147, 11.121064));
+		d.add(new GeoPoint(46.087546, 11.124969));
+		d.add(new GeoPoint(	46.085581, 11.125141));
+		d.add(new GeoPoint(46.080819, 11.126342));
+		e.add(new GeoPoint(	46.092844, 11.114541));
+		e.add(new GeoPoint(46.092606, 11.112824));
+		e.add(new GeoPoint(46.091445, 11.113167));
+		f.add(new GeoPoint(46.094123, 11.120635));
+		f.add(new GeoPoint(46.093915, 11.117931));
+		f.add(new GeoPoint(46.092457, 11.118746));
+		g.add(new GeoPoint(46.087510, 11.124942));
+		g.add(new GeoPoint(46.091365, 11.122239));
+		g.add(new GeoPoint(46.094906, 11.121359));
+		
+		h.add(new GeoPoint(46.095546, 11.120114));
+		h.add(new GeoPoint(46.094088, 11.120715));
+		h.add(new GeoPoint(46.091127, 11.120758));
+		
+		i.add(new GeoPoint(46.092912, 11.120694));
+		i.add(new GeoPoint(46.092034, 11.117110));
+		i.add(new GeoPoint(46.090517, 11.118183));
+		
+		j.add(new GeoPoint(46.094862, 11.110373));
+		j.add(new GeoPoint(46.094549, 11.109638));
+		j.add(new GeoPoint(46.092831, 11.110845));
+		j.add(new GeoPoint(46.092942, 11.111327));
+		j.add(new GeoPoint(46.092392, 11.111644));
+		j.add(new GeoPoint(46.092332, 11.111258));
+		j.add(new GeoPoint(46.090116, 11.113147));
+		
+		MyPolyline pa = new MyPolyline(getActivity());
+		pa.setPoints(a);
+		
+		MyPolyline pb = new MyPolyline(getActivity());
+		pb.setPoints(b);
+		
+		MyPolyline pc = new MyPolyline(getActivity());
+		pc.setPoints(c);
+		
+		MyPolyline pd = new MyPolyline(getActivity());
+		pd.setPoints(d);
+		
+		MyPolyline pe = new MyPolyline(getActivity());
+		pe.setPoints(e);
+		
+		MyPolyline pf = new MyPolyline(getActivity());
+		pf.setPoints(f);
+		
+		MyPolyline pg = new MyPolyline(getActivity());
+		pg.setPoints(g);
+		
+		MyPolyline ph = new MyPolyline(getActivity());
+		ph.setPoints(h);
+		
+		MyPolyline pi = new MyPolyline(getActivity());
+		pi.setPoints(i);
+		
+		MyPolyline pj = new MyPolyline(getActivity());
+		pj.setPoints(j);		
+		
+		MyPolyline pk = MyPolyline.decode(getActivity(),"qshxGatwbAqZdXw`@f^VaBtRsQxFwGzDmP|@eDxBcAr@nAg@lB}BHqDkCqCoJkDyN" );
+		
+		FolderOverlay fo = new FolderOverlay(mv.getContext());
+		fo.add(pa);
+		fo.add(pb);
+		fo.add(pc);
+		fo.add(pd);
+		fo.add(pe);
+		fo.add(pf);
+		fo.add(pg);
+		fo.add(ph);
+		fo.add(pi);
+		fo.add(pj);
+		fo.add(pk);
+		mv.getOverlays().add(fo);
+		
+		
+	}
 }
--- src/smartcampus/vas/parcheggiausiliari/android/activity/MyPolyline.java
+++ src/smartcampus/vas/parcheggiausiliari/android/activity/MyPolyline.java
@@ -1,14 +1,18 @@
 package smartcampus.vas.parcheggiausiliari.android.activity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.bonuspack.overlays.Polyline;
 import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class MyPolyline extends Polyline {
 
@@ -28,51 +32,112 @@ public class MyPolyline extends Polyline {
 		for (int i = 0; i < getNumberOfPoints() - 1; i++) {
 			GeoPoint a = points.get(i);
 			GeoPoint b = points.get(i + 1);
-				toRtn = toRtn || (linePointDist(a, b, point, true)<= 0.0005);
-
+			toRtn = toRtn || (linePointDist(a, b, point, true) <= 0.0005);
 		}
 		return toRtn;
 	}
 
+	@Override
+	public boolean onSingleTapUp(MotionEvent e, MapView mapView) {
+		// TODO Auto-generated method stub
+		if (isOnLine(mapView.getProjection().fromPixels((int) e.getX(),
+				(int) e.getX())))
+			// Log.d("DEBUG", "Path Pressed");
+			Toast.makeText(mapView.getContext(), "PATH PRESSED",
+					Toast.LENGTH_SHORT).show();
+		return super.onLongPress(e, mapView);
+	}
+
+	private double dot(IGeoPoint A, IGeoPoint B, IGeoPoint C) {
+		double[] AB = new double[2];
+		double[] BC = new double[2];
+		AB[0] = B.getLongitude() - A.getLongitude();
+		AB[1] = B.getLatitude() - A.getLatitude();
+		BC[0] = C.getLongitude() - B.getLongitude();
+		BC[1] = C.getLatitude() - B.getLatitude();
+		double dot = AB[0] * BC[0] + AB[1] * BC[1];
+		return dot;
+	}
+
+	// Compute the cross product AB x AC
+	private double cross(IGeoPoint A, IGeoPoint B, IGeoPoint C) {
+		double[] AB = new double[2];
+		double[] AC = new double[2];
+		AB[0] = B.getLongitude() - A.getLongitude();
+		AB[1] = B.getLatitude() - A.getLatitude();
+		AC[0] = C.getLongitude() - A.getLongitude();
+		AC[1] = C.getLatitude() - A.getLatitude();
+		double cross = AB[0] * AC[1] - AB[1] * AC[0];
+		return cross;
+	}
+
+	// Compute the distance from A to B
+	private double distance(IGeoPoint A, IGeoPoint B) {
+		double d1 = A.getLongitude() - B.getLongitude();
+		double d2 = A.getLatitude() - B.getLatitude();
+		return Math.sqrt(d1 * d1 + d2 * d2);
+	}
+
+	// Compute the distance from AB to C
+	// if isSegment is true, AB is a segment, not a line.
+	private double linePointDist(IGeoPoint A, IGeoPoint B, IGeoPoint C,
+			boolean isSegment) {
+		double dist = cross(A, B, C) / distance(A, B);
+		if (isSegment) {
+			double dot1 = dot(A, B, C);
+			if (dot1 > 0)
+				return distance(B, C);
+			double dot2 = dot(B, A, C);
+			if (dot2 > 0)
+				return distance(A, C);
+		}
+		return Math.abs(dist);
+	}
+
+	private static List<GeoPoint> decodePoly(String encoded) {
+		List<GeoPoint> poly = new ArrayList<GeoPoint>();
+
+		int index = 0, len = encoded.length();
+		int lat = 0, lng = 0;
+
+		while (index < len) {
+			int b, shift = 0, result = 0;
+			do {
+				b = encoded.charAt(index++) - 63;
+				result |= (b & 0x1f) << shift;
+				shift += 5;
+			} while (b >= 0x20);
+			int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+			lat += dlat;
+
+			shift = 0;
+			result = 0;
+			if (index >= len) {
+				break;
+			}
+			do {
+				b = encoded.charAt(index++) - 63;
+				result |= (b & 0x1f) << shift;
+				shift += 5;
+			} while (b >= 0x20);
+			int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+			lng += dlng;
+
+			GeoPoint p = new GeoPoint((int) (((double) lat / 1E5) * 1E6),
+					(int) (((double) lng / 1E5) * 1E6));
+			poly.add(p);
+		}
+
+		return poly;
+	}
 	
+	public static MyPolyline decode(Context ctx, String encoded)
+	{
+		MyPolyline toRtn = new MyPolyline(ctx);
+		toRtn.setPoints(decodePoly(encoded));
+		for(GeoPoint p : toRtn.getPoints())
+			Log.d("DECODED", ""+p.getLatitude()+"    "+p.getLongitude());
+		return toRtn;
+	}
 	
-	double dot(IGeoPoint A, IGeoPoint B, IGeoPoint C){
-		 	double[] AB = new double[2];
-		 	double[] BC = new double[2];
-	        AB[0] = B.getLongitude()-A.getLongitude();
-	        AB[1] = B.getLatitude()-A.getLatitude();
-	        BC[0] = C.getLongitude()-B.getLongitude();
-	        BC[1] = C.getLatitude()-B.getLatitude();
-	        double dot = AB[0] * BC[0] + AB[1] * BC[1];
-	        return dot;
-	    }
-	    //Compute the cross product AB x AC
-	double cross(IGeoPoint A, IGeoPoint B, IGeoPoint C){
-	    	double[] AB = new double[2];
-	    	double[] AC = new double[2];
-	        AB[0] = B.getLongitude()-A.getLongitude();
-	        AB[1] = B.getLatitude()-A.getLatitude();
-	        AC[0] = C.getLongitude()-A.getLongitude();
-	        AC[1] = C.getLatitude()-A.getLatitude();
-	        double cross = AB[0] * AC[1] - AB[1] * AC[0];
-	        return cross;
-	    }
-	    //Compute the distance from A to B
-	    double distance(IGeoPoint A, IGeoPoint B){
-	    	double d1 = A.getLongitude() - B.getLongitude();
-	    	double d2 = A.getLatitude() - B.getLatitude();
-	        return Math.sqrt(d1*d1+d2*d2);
-	    }
-	    //Compute the distance from AB to C
-	    //if isSegment is true, AB is a segment, not a line.
-	    double linePointDist(IGeoPoint A, IGeoPoint B, IGeoPoint C, boolean isSegment){
-	    		double dist = cross(A,B,C) / distance(A,B);
-	        	if(isSegment){
-	        	double dot1 = dot(A,B,C);
-	            if(dot1 > 0)return distance(B,C);
-	            double dot2 = dot(B,A,C);
-	            if(dot2 > 0)return distance(A,C);
-	        }
-	        return Math.abs(dist);
-	    }
 }
